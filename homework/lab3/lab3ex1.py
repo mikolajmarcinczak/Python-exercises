05f7d1472ac3004a7f2e3170d99f2b18b190d00a
mikolajmarcinczak <mikolajmarcinczak@users.noreply.github.com>
2021-04-04 11:56:30 +0200

    lab 3 exercises

---
+++ homework/lab3/lab3ex1.py
@@ -0,0 +1,19 @@
+﻿def Draw(width, height, char='*'):
+    Bar(width, char)
+    print()
+    for i in range(height - 2):
+        for j in range(width):
+            if j == 0:
+                print(char, end=" ")
+            elif j == width - 1:
+                print(char)
+                break
+            else:
+                print(' ', end=" ")
+    Bar(width, char)
+
+def Bar(width, char):
+    for i in range(width):
+        print(char, end=" ")
+
+Draw(12, 8, '%')
---
+++ homework/lab3/lab3ex2.py
@@ -0,0 +1,22 @@
+﻿def Sequence(start, n, diff):
+    if n == 1:
+        return start
+    elif n > 1:
+        return Sequence(start, n-1, diff) + diff
+    else:
+        return 'Ciąg nie może mieć wyrazu niedodatniego!'
+
+def UserInput():
+    while True:
+        try:
+            start = float(input('Podaj wyraz początkowy ciągu: '))
+            diff = float(input('Podaj różnicę ciągu: '))
+            n = int(input('Który wyraz ciągu chcesz odnaleźć? '))
+            break
+        except ValueError:
+            print('Podana wartość nie jest liczbą! Podaj wartości jeszcze raz!')
+            continue
+    return start, n, diff
+
+start, n, diff = UserInput()
+print(f'{n}-ty wyraz ciągu to: {Sequence(start, n, diff)}')
