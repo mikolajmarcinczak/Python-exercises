f79b244b5c5a0e5f8429c3e683885f8e657c75c6
Mikołaj <mikolajtechhnika@gmail.com>
2021-04-21 21:14:43 +0200

    lab4 homework

--- homework/lab4/lab4ex1.py
+++ homework/lab4/lab4ex1.py
@@ -1,1 +1,16 @@
-﻿
+﻿import numpy as np
+file = open('../../zajecia 4/pliki/projektowe.txt', 'r')
+text = file.read()
+
+def LetterCount(text):
+    counter = []
+    alpha = np.array([chr(i) for i in range(65, 91)] + [chr(i) for i in range(97, 123)]) 
+    for letter in alpha:
+        counter.append(text.count(letter))
+    return alpha, counter
+
+if __name__ == '__main__':
+    a, c = LetterCount(text)
+    for k, v in zip(a, c):
+        print(f'[{k}] : [{v}]')
+    print(f'Liczba znaków w pliku // The no. of characters in the file: {len(text)}\nLiczba znaków w pliku(wyłączając spacje) // The no. of characters in the file(excl. spaces): {len(text.replace(" ",""))}')
--- homework/lab4/lab4ex2.py
+++ homework/lab4/lab4ex2.py
@@ -1,1 +1,21 @@
-﻿
+﻿import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+numbers = []
+for i in range(10001):
+    numbers.append(random.randint(1,10))
+
+distinct = np.array([int(x) for x in range(1, 11)])
+
+values = []
+for element in distinct:
+    values.append(numbers.count(element))
+
+plt.figure(figsize=(10, 5))
+plt.bar(distinct, values, color='green')
+
+for index, data in enumerate(values):
+    plt.text(index + 0.8, data/values[index] + 10, s=f'{data}')
+
+plt.show()
